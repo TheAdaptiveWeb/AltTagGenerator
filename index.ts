96679c4c0b37e28616b9363634d86fae02fb6d22
Adam Hirst <adam@ahirst.com>
2019-04-01 19:48:06 +0100

    Initial

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { AdapterContext } from 'adaptiveweb';
+declare const aw: AdapterContext;
+
+/**
+ * You can reference the AdapterContext with `aw.[context method]`.
+ * 
+ * For example, to get preferences, use:
+ * ```
+ * aw.getPreferences().then((preferences: any) => {
+ *    // Do something with the preferences
+ * });
+ * ```
+ * 
+ * For full docs, see https://github.com/TheAdaptiveWeb/docs
+ */
+
+console.log('Hello, Adaptive Web!');
